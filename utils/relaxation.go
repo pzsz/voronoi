4c65303adc702494da60b348cc24d55640caf17e
Przemyslaw Szczepaniak <przeszczep@gmail.com>
2013-05-09 22:14:15 +0100

    Added utility for finding if point is inside of cell

--- utils/relaxation.go
+++ utils/relaxation.go
@@ -10,32 +10,6 @@ import (
 	"github.com/pzsz/voronoi"
 )
 
-// Calculate area of a cell
-func CellArea(cell *voronoi.Cell) float64 {
-	area := float64(0)
-	for _, halfedge := range cell.Halfedges {
-		s := halfedge.GetStartpoint()
-		e := halfedge.GetEndpoint()
-		area += s.X * e.Y
-		area -= s.Y * e.X
-	}
-
-	return area/2
-}
-
-// Calculate centroid of a cell
-func CellCentroid(cell *voronoi.Cell) voronoi.Vertex {
-	x, y := float64(0), float64(0)
-	for _, halfedge := range cell.Halfedges {
-		s := halfedge.GetStartpoint()
-		e := halfedge.GetEndpoint()
-		v := s.X*e.Y - e.X*s.Y
-		x += (s.X+e.X) * v
-		y += (s.Y+e.Y) * v
-	}
-	v := CellArea(cell) * 6
-	return voronoi.Vertex{x/v, y/v}
-}
 
 // Apply lloyd relaxation algorithm to the cells.
 func LloydRelaxation(cells []*voronoi.Cell) (ret []voronoi.Vertex) {
